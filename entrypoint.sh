d9b9a4e67e4ae516423d7fe5f76cf5f4c708ca07
chakravartyk <kchakravarty@deloitte.com>
2023-01-05 11:42:13 +0530

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,5 @@ curl --silent https://api.github.com/meta | \
   python3 -c 'import json,sys;print(*["github.com " + x for x in json.load(sys.stdin)["ssh_keys"]], sep="\n")' \
   >> ~/.ssh/known_hosts
 chmod 0600 ~/.ssh/known_hosts
-sudo curl -Ls https://raw.githubusercontent.com/gruntwork-io/gruntwork-installer/"$1"/bootstrap-gruntwork-installer.sh | bash /dev/stdin --version "$1"
+curl -Ls https://raw.githubusercontent.com/gruntwork-io/gruntwork-installer/"$1"/bootstrap-gruntwork-installer.sh | bash /dev/stdin --version "$1"
 gruntwork-install --binary-name 'boilerplate' --repo 'https://github.com/gruntwork-io/boilerplate' --tag "$2" --no-sudo
